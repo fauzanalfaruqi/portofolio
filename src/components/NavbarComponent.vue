37378d5b953f6b7af659e5aa2306f9c2c1f366a4
fauzanalfaruqi <fauzanalfaruqi@gmail.com>
2025-03-19 03:02:32 +0700

    Implement responsive styling for small screen, only use regular cv to showcase

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,43 +1,57 @@
 <template>
     <div class="navbar pt-[10px]">
-        <div class="flex flex-row items-center justify-end max-w-screen-xl px-4 mx-auto sm:px-6">
-            <router-link :class="{'active-nav-link': $route.path === '/', 'nav-link': $route.path !== '/'}" to="/">Home</router-link>
-            <router-link :class="{'active-nav-link': $route.path === '/about', 'nav-link': $route.path !== '/about'}" to="/about">About</router-link>
-            <router-link :class="{'active-nav-link': $route.path === '/projects', 'nav-link': $route.path !== '/projects'}" to="/projects">Projects</router-link>
-            <router-link :class="{'active-nav-link': $route.path === '/contact', 'nav-link': $route.path !== '/contact'}" to="/contact">Contact</router-link>
+        <div class="max-w-screen-xl px-4 mx-auto sm:px-6">
+            <!-- Desktop Navbar -->
+            <div class="hidden md:flex flex-row items-center justify-end">
+                <router-link :class="getNavClass('/')" to="/">Home</router-link>
+                <router-link :class="getNavClass('/about')" to="/about">About</router-link>
+                <router-link :class="getNavClass('/projects')" to="/projects">Projects</router-link>
+                <router-link :class="getNavClass('/contact')" to="/contact">Contact</router-link>
+            </div>
+
+            <!-- Mobile Navbar -->
+            <div class="md:hidden flex justify-between items-center">
+                <button @click="toggleMenu" class="hamburger-btn">
+                    ☰
+                </button>
+            </div>
+
+            <!-- Dropdown Menu for Mobile -->
+            <div v-if="isMenuOpen" class="mobile-menu">
+                <router-link @click="closeMenu" :class="getNavClass('/')" to="/">Home</router-link>
+                <router-link @click="closeMenu" :class="getNavClass('/about')" to="/about">About</router-link>
+                <router-link @click="closeMenu" :class="getNavClass('/projects')" to="/projects">Projects</router-link>
+                <router-link @click="closeMenu" :class="getNavClass('/contact')" to="/contact">Contact</router-link>
+            </div>
         </div>
-        <hr>
+        <hr />
     </div>
 </template>
 
 <script>
 export default {
-    name: 'NavbarComponent'
-}
+    name: 'NavbarComponent',
+    data() {
+        return {
+            isMenuOpen: false
+        };
+    },
+    methods: {
+        toggleMenu() {
+            this.isMenuOpen = !this.isMenuOpen;
+        },
+        closeMenu() {
+            this.isMenuOpen = false;
+        },
+        getNavClass(route) {
+            return this.$route.path === route ? 'active-nav-link' : 'nav-link';
+        }
+    }
+};
 </script>
 
 <style>
-
-.nav-link {
-    color: #3F94A7;
-    font-size: 16px;
-    font-weight:400;
-    text-decoration: none;
-    text-align: center;
-    padding: 0 25px;
-}
-
-.active-nav-link {
-    background-color: #3F94A7;
-    color: #ffffff;
-    font-size: 16px;
-    font-weight:400;
-    text-decoration: none;
-    text-align: center;
-    padding: 5px 25px;
-    cursor: default;
-}
-
+/* General Styles */
 hr {
     border-style: solid;
     border-width: 1px;
@@ -45,13 +59,71 @@ hr {
     margin: 0;
 }
 
+.nav-link {
+    color: #3F94A7;
+    font-size: 16px;
+    font-weight: 400;
+    text-decoration: none;
+    text-align: center;
+    padding: 0 25px;
+    transition: transform 0.2s ease-in-out;
+}
+
+.active-nav-link {
+    background-color: #3F94A7;
+    color: #ffffff;
+    font-size: 16px;
+    font-weight: 400;
+    text-decoration: none;
+    text-align: center;
+    padding: 5px 25px;
+    cursor: default;
+}
+
 .nav-link:hover {
     transform: scale(1.1);
 }
 
-@media only screen and (min-width: 1600px) {
-    .nav-container {
-        margin: 0 250px;
+/* Mobile Menu Styles */
+.hamburger-btn {
+    font-size: 24px;
+    background: none;
+    border: none;
+    cursor: pointer;
+    color: #3F94A7;
+    padding: 10px;
+}
+
+.mobile-menu {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: white;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    position: absolute;
+    top: 50px;
+    left: 0;
+    right: 0;
+    z-index: 10;
+    padding: 10px;
+}
+
+.mobile-menu .nav-link {
+    padding: 10px;
+    width: 100%;
+    text-align: center;
+}
+
+/* Responsive Styles */
+@media (max-width: 768px) {
+    .mobile-menu {
+        display: flex;
+    }
+}
+
+@media (min-width: 769px) {
+    .mobile-menu {
+        display: none;
     }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,4 @@
 <template>
-  <!-- <section class="min-h-[85vh]">
-    <div class="relative flex flex-col flex-1 items-center max-w-screen-xl px-6 mx-auto md:flex-row sm:px-6 p-8 h-full">
-      <div class="flex flex-col flex-1 justify-start relative w-1/2">
-        <h1>M. Fauzan Al-Faruqi</h1>
-        <div id="location">
-          <img src="../assets/pin-map-logo.png" alt="pin-map">
-          <p id="based-location">Sukabumi City, West Java</p>
-        </div>
-        <p id="description">An IT enthusiast proficient in backend development
-          using Go, and have years of experience in game
-          development utilizing Unity Engine.</p>
-        <button-component :style="'primary'" @click="showModal">Download CV
-        </button-component>
-        <router-link to="/projects">
-          <button-component :style="'secondary'">See My Projects
-          </button-component>
-        </router-link>
-      </div>
-      <div class="flex flex-col flex-1 justify-end align-center w-1/2">
-        <img src="../assets/profile.png" alt="profile" width="320px">
-      </div>
-    </div>
-    <modal-component :isOpen="modalIsOpen" @close="closeModal">
-      <download-cv-modal-component :enigmaCVLink="'https://fa-portofolio.web.app/enigma_resume.pdf'"
-        :regularCVLink="'https://fa-portofolio.web.app/resume.pdf'"
-        :fileIconLink="'https://fa-portofolio.web.app/icons/pdf_icon.png'">
-
-      </download-cv-modal-component>
-    </modal-component>
-  </section> -->
-
   <div class="relative flex flex-col-reverse items-center max-w-screen-xl px-4 mx-auto md:flex-row sm:px-6 p-8 h-[85vh] 2xl:h-[87.5vh]">
     <div class="flex items-center py-5 md:w-1/2 md:pb-20 md:pt-10 md:pr-10">
       <div class="text-center md:text-left">
@@ -43,7 +12,7 @@
           utilizing Unity Engine.
         </p>
         <div class="mt-6 sm:flex gap-2">
-          <button-component :style="'primary'" @click="showModal">Download CV
+          <button-component class="mb-2" :style="'primary'" @click="onDownloadClicked">Download CV
           </button-component>
           <router-link to="/projects">
             <button-component :style="'secondary'">See My Projects
@@ -95,6 +64,10 @@
 
       closeModal() {
         this.modalIsOpen = false;
+      },
+
+      onDownloadClicked() {
+        window.open('https://fa-githubio.web.app/static/resume.pdf', '_blank');
       }
     }
   }
@@ -107,62 +80,6 @@
     padding: 40px 20px;
     margin: 20px 150px;
   }
-/*   
-  .left-content {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    position: relative;
-  }
-   */
-  /* .right-content {
-    flex: 1;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-  }
-
-  .fixed-footer {
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-  }
-
-  #location {
-    display: flex;
-    flex-direction: row;
-    margin-top: 5px;
-    margin-bottom: 0px;
-  }
-
-  #location img {
-    width: 14px;
-    height: 20px;
-    margin-top: 2px;
-    margin-right: 7.5px;
-  }
-
-  #description {
-    margin-top: 0px;
-  }
-   */
-  /* h1 {
-    margin-bottom: 0;
-    font-size: 36px;
-    color: #5A5A5A;
-  } */
-/*   
-  p {
-    font-size: 18px;
-    color: #5A5A5A;
-  } */
-/*   
-  #based-location {
-    color: #3F94A7;
-    margin-top: 0px;
-  } */
 
   </style>
   
